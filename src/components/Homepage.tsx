a4f41760f6a62100d7296153e5b4bbf2c01f3952
Marius Avram <marius.avram1309@gmail.com>
2021-12-04 20:15:11 +0200

    Create first component
    Will be a container for other components

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Homepage(): JSX.Element {
+    return (
+        <div>
+            <p>This is where the content is going to be.</p>
+        </div>
+    )
+}
